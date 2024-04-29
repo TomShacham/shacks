b3e85a0c604bbaf2e430d08a210f70fa8e21a240
tom <shachamtom@gmail.com>
2024-04-29 13:43:35 +0100

    example app using latest published h22p package

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import {h22p, route, router} from '@shacks/h22p'
 
 async function main() {
-    const {server, port, close} = await h22p.server(router([
+    console.log(process.env.PORT);
+    const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+    const {server, close} = await h22p.server(router([
         route('GET', '/{id}', async (req) => {
             return h22p.response({body: req.vars.path.id})
         })
-    ]), 3000);
+    ]), port);
     console.log(`Running on port ${port}`);
 }
 
