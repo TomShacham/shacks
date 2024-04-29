d83c8331e04e9f2bea55f06bf82ff3b40d6b4a56
tom <shachamtom@gmail.com>
2024-04-29 14:16:07 +0100

    allow server host to be configurable

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
 import {h22p, route, router} from '@shacks/h22p'
 
 async function main() {
-    console.log(process.env.PORT);
     const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+    const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1';
+
+    console.log({port, host});
+
     const {server, close} = await h22p.server(router([
         route('GET', '/{id}', async (req) => {
             return h22p.response({body: req.vars.path.id})
         })
-    ]), port);
+    ]), port, host);
     console.log(`Running on port ${port}`);
 }
 
