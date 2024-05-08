ceefbde1db3333a983d353ebb79de3818b5350a0
tom <shachamtom@gmail.com>
2024-05-08 15:19:13 +0100

    status enum for convenience

--- src/packages/h22p/test/body.spec.ts
+++ src/packages/h22p/test/body.spec.ts
@@ -2,7 +2,7 @@ import * as stream from "stream";
 import {expect} from "chai";
 import {Body, MultipartForm} from "../src/body";
 import * as fs from "fs";
-import {h22p} from "../src/interface";
+import {h22p, Status} from "../src/interface";
 
 describe('body', () => {
 
@@ -17,7 +17,7 @@ describe('body', () => {
     })
 
     describe('response helpers', () => {
-        it('doesnt let you set the status, or statusText its always 200', () => {
+        it('doesnt let you set the status, or statusText; redirects have to provide a location', () => {
             // cannot pass status or statusText in as a property
             expect(h22p.ok().status).eq(200);
             expect(h22p.ok().statusText).eq('OK');
@@ -110,6 +110,71 @@ describe('body', () => {
             expect(h22p.gatewayTimeout().status).eq(504);
             expect(h22p.gatewayTimeout().statusText).eq('Gateway Timeout');
 
+        })
+
+        it('status enum', () => {
+            expect(Status.continue).eq(100);
+            expect(Status.switchingProtocols).eq(101);
+            expect(Status.processing).eq(102);
+            expect(Status.earlyHints).eq(103);
+            expect(Status.ok).eq(200);
+            expect(Status.created).eq(201);
+            expect(Status.accepted).eq(202);
+            expect(Status.nonAuthoritativeInformation).eq(203);
+            expect(Status.noContent).eq(204);
+            expect(Status.resetContent).eq(205);
+            expect(Status.partialContent).eq(206);
+            expect(Status.multiStatus).eq(207);
+            expect(Status.alreadyReported).eq(208);
+            expect(Status.imUsed).eq(226);
+            expect(Status.multipleChoices).eq(300);
+            expect(Status.movedPermanently).eq(301);
+            expect(Status.found).eq(302);
+            expect(Status.seeOther).eq(303);
+            expect(Status.notModified).eq(304);
+            expect(Status.useProxy).eq(305);
+            expect(Status.temporaryRedirect).eq(307);
+            expect(Status.permanentRedirect).eq(308);
+            expect(Status.badRequest).eq(400);
+            expect(Status.unauthorized).eq(401);
+            expect(Status.paymentRequired).eq(402);
+            expect(Status.forbidden).eq(403);
+            expect(Status.notFound).eq(404);
+            expect(Status.methodNotAllowed).eq(405);
+            expect(Status.notAcceptable).eq(406);
+            expect(Status.proxyAuthenticationRequired).eq(407);
+            expect(Status.requestTimeout).eq(408);
+            expect(Status.conflict).eq(409);
+            expect(Status.gone).eq(410);
+            expect(Status.lengthRequired).eq(411);
+            expect(Status.preconditionFailed).eq(412);
+            expect(Status.payloadTooLarge).eq(413);
+            expect(Status.uriTooLong).eq(414);
+            expect(Status.unsupportedMediaType).eq(415);
+            expect(Status.rangeNotSatisfiable).eq(416);
+            expect(Status.expectationFailed).eq(417);
+            expect(Status.imATeapot).eq(419);
+            expect(Status.misdirectedRequest).eq(421);
+            expect(Status.unprocessableEntity).eq(422);
+            expect(Status.locked).eq(423);
+            expect(Status.failedDependency).eq(424);
+            expect(Status.tooEarly).eq(425);
+            expect(Status.upgradeRequired).eq(426);
+            expect(Status.preconditionRequired).eq(428);
+            expect(Status.tooManyRequests).eq(429);
+            expect(Status.requestHeaderFieldsTooLarge).eq(431);
+            expect(Status.unavailableForLegalReasons).eq(451);
+            expect(Status.internalServerError).eq(500);
+            expect(Status.notImplemented).eq(501);
+            expect(Status.badGateway).eq(502);
+            expect(Status.serviceUnavailable).eq(503);
+            expect(Status.gatewayTimeout).eq(504);
+            expect(Status.httpVersionNotSupported).eq(505);
+            expect(Status.variantAlsoNegotiates).eq(506);
+            expect(Status.insufficientStorage).eq(507);
+            expect(Status.loopDetected).eq(508);
+            expect(Status.notExtended).eq(510);
+            expect(Status.networkAuthenticationRequired).eq(511);
         })
     })
 
