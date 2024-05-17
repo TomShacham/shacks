1d521b2ab5daaca3944def3d320795a03b45e0fe
tom <shachamtom@gmail.com>
2024-05-17 14:54:15 +0100

    wip routing - type-safe routing in memory works end-to-end

--- src/packages/h22p/test/router3.spec.ts
+++ src/packages/h22p/test/router3.spec.ts
@@ -1,3 +1,4 @@
+import {expect} from "chai";
 import {JsonBody} from "../src";
 
 type pathParameterToString<Part> = Part extends `{${infer Name}}` ? string : Part;
@@ -41,31 +42,61 @@ type routes<
         : handler<Uri, ReqB, ResB>
 }
 
-type x = '/resource/{id}'
-type y = backToPath<x>
 
 function get<
     Uri extends string,
     ReqB extends JsonBody,
     ResB extends JsonBody,
->(uri: Uri, body: ReqB, handler: handler<backToPath<Uri>, ReqB, ResB>): handler<backToPath<Uri>, ReqB, ResB> {
-    return {handle: (req: req<backToPath<Uri>, ReqB>) => handler.handle(req)}
+>(uri: Uri, body: ReqB, handler: handler<backToPath<Uri>, ReqB, ResB>): {
+    handler: handler<backToPath<Uri>, ReqB, ResB>,
+    req: (uri: backToPath<Uri>, body: ReqB) => req<backToPath<Uri>, ReqB>
+} {
+    return {
+        handler: {handle: (req: req<backToPath<Uri>, ReqB>) => handler.handle(req)},
+        req: (uri, body) => ({uri, body})
+    }
 }
 
+type x = { getResource: handler<`/resource/${string}/sub/${string}`, { foo: { bar: string } }, { bar: string }> };
+type y = x extends routes<{}, string, JsonBody, JsonBody> ? true : false;
+
 const routes = {
-    getResource: get('/resource/{id}/sub/{subId}', {foo: {bar: 'json'}} as const, handle(async (req) => {
+    getResource: get('/resource/{id}/sub/{subId}', {foo: {bar: 'json'}}, handle(async (req) => {
         const u = req.uri
         return {status: 200, body: {bar: 'json'}}
     })),
+    postResource: get('/resource/{id}', {foo: {baz: 'json'}}, handle(async (req) => {
+        const u = req.uri
+        return {status: 200, body: {quux: 'json'}}
+    }))
 };
 
 describe('test', () => {
-    it('does a thing', async () => {
-        const response = await routes.getResource.handle({uri: '/resource/123/sub/456', body: {foo: {bar: 'json'}}});
+    it('handle an in-memory type-safe request and response', async () => {
+        const response = await routes.getResource.handler.handle({
+            uri: '/resource/123/sub/456',
+            body: {foo: {bar: 'json'}}
+        });
         response.body.bar
         // @ts-expect-error -- foo does not exist on type body
         response.body.foo
-
+    })
+    it('build a type-safe request', async () => {
+        const typeSafeRequest = routes.getResource.req(
+            '/resource/123/sub/456',
+            {foo: {bar: 'json'}}
+        );
+        expect(typeSafeRequest).deep.eq({uri: '/resource/123/sub/456', body: {foo: {bar: 'json'}}})
+        const doesntTypeCheck = routes.getResource.req(
+            // @ts-expect-error
+            '/resource/123/sub',
+            {foo: {bar: 'json'}}
+        );
+        const alsoDoesntTypeCheck = routes.getResource.req(
+            '/resource/123/sub/456',
+            // @ts-expect-error
+            {foo: {bar: 123}}
+        );
     })
 })
 
