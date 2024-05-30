32407ff1fcb6ee108645b83dc889f327bae5de8d
tom <shachamtom@gmail.com>
2024-05-30 15:49:22 +0100

    build - make build file work when executing from any dir

--- src/packages/h22p/build.ts
+++ src/packages/h22p/build.ts
@@ -2,6 +2,24 @@
 import * as Bun from 'bun';
 import * as fs from "fs";
 
+const entrypoints = [
+    `/src/browser-index.ts`,
+    `/test/resources/app.ts`
+];
+
+Bun.build({
+    entrypoints: entrypoints.map(entry => `${__dirname}${entry}`),
+    outdir: './bun',
+    target: "browser"
+}).then(r => {
+    if (!r.success) throw new Error(`Failed to compile \n ${r.logs.join('\n')}`)
+    entrypoints.forEach(path => {
+        const pathToBuiltJs = path.replace('.ts', '.js');
+        return bunHotFixes(`./bun${pathToBuiltJs}`);
+    });
+}).then(d => console.log('compiled'));
+
+
 function bunHotFixes(filePath: string) {
     let output = fs.readFileSync(filePath, 'utf-8');
     output = output.replaceAll('extends undefined', '');
@@ -12,16 +30,4 @@ function bunHotFixes(filePath: string) {
   }`, '');
     fs.writeFileSync(filePath, output, 'utf-8');
 }
-
-const entrypoints = ['/src/browser-index.ts', '/test/resources/app.ts'];
-Bun.build({
-    entrypoints: entrypoints,
-    outdir: './bun',
-    target: "browser"
-}).then(r => {
-    entrypoints.forEach(path => {
-        const pathToBuiltJs = path.replace('.ts', '.js');
-        return bunHotFixes(`./bun${pathToBuiltJs}`);
-    });
-}).then(d => console.log('compiled'));
 
